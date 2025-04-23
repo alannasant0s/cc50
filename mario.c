1af65d6e7416c41600424f74fae8496293d4f3f0
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-04-23 14:22:47 -0300

    Exercicio do mario

---
+++ mario.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) 
+{
+     int x = get_int("x: ");
+ 
+     int y = get_int("y: ");
+
+     printf("%i\n", x + y); 
+}
+
+
+i = 0; i < 3; i++)
+    {
+        printf("meow\n");
+    }
+    return 0;
+}
