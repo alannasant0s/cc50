6ccb021775bb5e16ae4dc6917900857473c5240c
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-05-06 15:22:10 -0300

    Código de alocação de memória

---
+++ memory.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>     
+
+int main(void)
+{
+    int *x = malloc(3 * sizeof(int)); // Guarda na memoria 3 inteiros
+    x[0] = 72;
+    x[1] = 73;      
+    x[2] = 74;
+    free(x); // Libera a memoria alocada para o vetor x
+}
